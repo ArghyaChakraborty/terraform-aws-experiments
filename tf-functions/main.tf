24478d86be2823876e529d4357c4502bceb1ffaa
Chakraborty <501954963@ge.com>
2022-09-07 21:23:23 -0500

    Added more function examples

--- tf-functions/main.tf
+++ tf-functions/main.tf
@@ -2,8 +2,11 @@ terraform {
   
 }
 
-output "math_max" {
+output "math_max_1" {
   value = max(2, 10, 1)
+}
+output "math_max_2" {
+  value = max([2, 10, 1]...)
 }
 output "math_min" {
   value = max(2, 10, 1)
@@ -19,8 +22,11 @@ output "math_z_break" {
   value = "----------------------------"
 }
 
-output "string_format" {
+output "string_format_1" {
   value = format("Hello %s, are you %d years old ?", "User", 20)
+}
+output "string_format_2" {
+  value = try(format("Hello %d", "World"), "Failed to format string")
 }
 output "string_formatlist" {
   value = formatlist("Hello %s, are you %d years old ?", ["Sheldon", "Amy"], [30, 25])
@@ -35,13 +41,28 @@ output "string_split" {
   value = split(",", "Sheldon,Raj,Amy,Penny")
 }
 output "string_regex_1" {
-  value = regex("^S.+n$", "Sheldon")
-}
-output "string_regex_2" {
   value = try(regex("^S.+n$", "Amy"), "No match found")
 }
-output "string_regexall" {
+output "string_regex_2" {
+  value = regex("^S.+n.+$", "Sheldon and Amy")
+}
+output "string_regex_3" {
+  value = regex(".+-(\\d\\d)-.+", "2022-09-01")
+}
+output "string_regex_4" {
+  value = regex(".+-(?P<month>\\d\\d)-.+", "2022-09-01")
+}
+output "string_regexall_1" {
   value = regexall("The", "The Big Bang Theory is The biggest show on Earth !!!")
+}
+output "string_regexall_2" {
+  value = regexall("(The)", "The Big Bang Theory is The biggest show on Earth !!!")
+}
+output "string_regexall_3" {
+  value = regexall("(?P<the>The)", "The Big Bang Theory is The biggest show on Earth !!!")
+}
+output "string_regexall_4" {
+  value = regexall("None", "The Big Bang Theory is The biggest show on Earth !!!")
 }
 output "string_replace" {
   value = replace("Raj was a better scientist", "Raj", "Sheldon")
@@ -52,11 +73,23 @@ output "string_title" {
 output "string_chomp" {
   value = chomp("\nThe Big Bang Theory\n\n")
 }
-output "string_trim" {
+output "string_trim_1" {
   value = trim(" \nThe Big Bang Theory\n\n", "\n")
+}
+output "string_trim_2" {
+  value = trim(" \nThe Big Bang Theory\n\n", "\n ")
+}
+output "string_trimprefix_1" {
+  value = trimprefix("\n The Big Bang Theory", " \n")
+}
+output "string_trimprefix_2" {
+  value = trimprefix("\n The Big Bang Theory", "\n ")
 }
 output "string_trimspace" {
   value = trimspace(" \n The Big Bang Theory\n\n ")
+}
+output "string_substr" {
+  value = substr("Hello World", -5, 100)
 }
 
 output "string_z_break" {
@@ -84,6 +117,9 @@ output "collection_coalesce_1" {
 output "collection_coalesce_2" {
   value = coalesce(null, "b", "c")
 }
+output "collection_coalesce_3" {
+  value = coalesce([null, 1, "c"]...)
+}
 output "collection_coalescelist_1" {
   value = coalescelist(["a"], ["b"], ["c"])
 }
@@ -95,6 +131,9 @@ output "collection_compact" {
 }
 output "collection_concat" {
   value = concat([], ["b"], ["c"])
+}
+output "collection_merge" {
+  value = merge({"a": 1}, {"b": 2, "a": 3})
 }
 output "collection_contains_1" {
   value = contains(["Sheldon", "Raj", "Amy"], "Raj")
@@ -113,6 +152,9 @@ output "collection_element_1" {
 }
 output "collection_element_2" {
   value = element(["Sheldon", "Raj", "Amy"], 3)
+}
+output "collection_element_3" {
+  value = element(["Sheldon", "Raj", "Amy"], length(["Sheldon", "Raj", "Amy"])-1)
 }
 output "collection_keys" {
   value = keys({"name": "Sheldon", "job": "Scientist"})
@@ -192,8 +234,11 @@ output "file_z_break" {
 output "datetime_timestamp" {
   value = timestamp()
 }
-output "datetime_formatdate" {
+output "datetime_formatdate_1" {
   value = formatdate("YYYY-MM-DD", timestamp())
+}
+output "datetime_formatdate_2" {
+  value = formatdate("HH' hour 'mm' min'", timestamp())
 }
 
 output "datetime_z_break" {
@@ -205,6 +250,9 @@ output "crypto_uuid" {
 }
 output "crypto_filesha512" {
   value = filesha512("./templatefile.tftpl")
+}
+output "crypto_sha512" {
+  value = sha512(file("./templatefile.tftpl"))
 }
 
 output "crypto_z_break" {
@@ -230,14 +278,35 @@ output "type_sensitive" {
 output "type_nonsensitive" {
   value = nonsensitive(sensitive("super secret"))
 }
+output "type_tobool_1" {
+  value = tobool("false")
+}
+output "type_tobool_2" {
+  value = try(tobool("wrong"), "Can not be converted to bool value")
+}
+output "type_type" {
+  value = try(type(["1", "2"]), "type() is available only via terraform console")
+}
 
 output "type_z_break" {
   value = "----------------------------"
 }
 
 
-output "network_TODO" {
-  value = "#TODO"
+output "network_cidrhost_1" {
+  value = cidrhost("10.0.0.0/28", 1)
+}
+output "network_cidrhost_2" {
+  value = cidrhost("10.0.0.0/28", 2)
+}
+output "network_cidrhost_3" {
+  value = cidrhost("10.0.0.0/28", 15)
+}
+output "network_cidrhost_4" {
+  value = try(cidrhost("10.0.0.0/28", 16), "Out of range error")
+}
+output "network_cidrsubnet" {
+  value = cidrsubnet("10.0.0.0/28", 2, 2)
 }
 
 output "network_z_break" {
