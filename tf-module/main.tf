f67f36d33676137403c8cb2bbbd9ded65a0190de
Chakraborty <501954963@ge.com>
2022-09-13 21:48:53 -0500

    Saving changes

--- tf-module/main.tf
+++ tf-module/main.tf
@@ -1,9 +1,19 @@
 /*
     Configure:
-        #TODO
-        git tag v0.0.1
-        git push --tags
-        
+        For using modules from GitHub
+            git add .
+            git commit -m "Initial Commit"
+            git push origin main
+            git tag vx.y.x
+            git push --tags
+            Also replace `vx.y.z` in git module example's source (value of ref parameter)
+        For using modules from Terraform registry
+            Login to https://app.terraform.io/ -> Registry -> Publish -> Module
+            Connect to VCS -> GitHub -> GitHub.com (custom) -> Follow the instructions provided
+            Also need to generate SSH keypair. You can use this online website for the same: https://travistidwell.com/jsencrypt/demo/
+            Copy the private part of the keypair in Terraform & keep the private & public parts with you
+            Once done, go to https://app.terraform.io/ again -> Registry -> Publish -> Module
+            Choose the registered GitHub -> 
     Execute:
         terraform init
         terraform apply -auto-approve
@@ -27,7 +37,7 @@ output "local_module_output" {
   value = module.local_module.module_output
 }
 
-# using tf module from public terraform repo
+# Using tf module from public terraform registry
 
 # Using tf module from public github repo
 module "git_module" {
@@ -48,8 +58,8 @@ output "local_module_output_with_count" {
   value = module.local_module_with_count.*.module_output
 }
 
-# Using for_each
-module "local_module_with_foreach" {
+# Using for_each with a map
+module "local_module_with_foreach_map" {
   for_each = {
     "Sheldon" = "Cooper"
     "Howard" = "Wolowitz" 
@@ -57,6 +67,16 @@ module "local_module_with_foreach" {
   source = "./modules/simple_module"
   name = "${each.key} ${each.value}"
 }
-output "local_module_output_with_foreach" {
-  value = module.local_module_with_foreach.*
+output "local_module_output_with_foreach_map" {
+  value = module.local_module_with_foreach_map.*
+}
+
+# Using for_each with a set
+module "local_module_with_foreach_set" {
+  for_each = toset(["Sheldon", "Amy", "Leonard", "Penny"])
+  source = "./modules/simple_module"
+  name = "${each.key} ${each.value}"
+}
+output "local_module_output_with_foreach_set" {
+  value = module.local_module_with_foreach_set.*
 }
